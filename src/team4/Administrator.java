81f6b1c48e4a5e87b9107344f318b30fe1d93ca1
1895321 <1895321@E21046.CEGEPGIM.LCL>
2019-02-17 09:36:33 -0500

    Person, Administrator and User classes are created

---
+++ src/team4/Administrator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team4;
+
+/**
+ *
+ * @author 1895321
+ */
+public class Administrator {
+    
+    String UserName,Password;
+    
+    
+    public Administrator(String UName,String Pass){
+    
+    this.UserName=UName;
+    this.Password=Pass;
+    
+    }
+    
+    public String getUserName()
+    
+}
