6ade313b9e848ee50cac746b96c8c133f8adf6e0
vinay <vinayraodugyala99@gmail.com>
2019-02-17 09:21:36 -0500

    created category class

---
+++ src/team4/category.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team4;
+import java .util.*;
+/**
+ *
+ * @author temp
+ */
+public class category
+{
+    String name;
+    String id;
+    String description;
+    
+    public category(String p_name,String p_id,String p_description )
+    {
+
+        this.name=p_name;
+        this.id = p_id;
+        this.description=p_description;
+
+    }
+    public String getName()
+    {
+        return name;
+    }
+public String getid()
+    {
+        return id;
+    }
+public String getdescription()
+    {
+        return description;
+    }
+public void setName(String p_name)
+{
+    String name = p_name;
+    
+}
+public void setid(String p_id)
+{
+    String id = p_id;
+    
+}
+public void setdescription(String p_description)
+{
+    String name = p_description;
+    
+}
+}
