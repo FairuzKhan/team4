df5e409d36acc668dc6b6b0e8eea2f762c8b2039
vinay <vinayraodugyala99@gmail.com>
2019-03-03 10:38:26 -0500

    changes in team4 and  details  class

--- src/team4/details.java
+++ src/team4/details.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package team4;
+import java.text.SimpleDateFormat;
 import java .util.*;
 /**
  *
@@ -11,12 +12,56 @@ import java .util.*;
  */
 public class details 
 {
+    String id;
     Date  dateAdd;
     
     public details(Date k_DateAdd)
     {
         this.dateAdd = k_DateAdd;
+        id = createID("");
         
+    }
+     public static String createID(String name) {
+         name="details";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMM-dd");
+        Date newDate = new Date();
+        String date = sdf.format(newDate);
+        String[] dt = date.split("-");
+        String dtInt;
+        if (Integer.parseInt(dt[2]) % 2 == 0)
+            dtInt = "E";
+        else
+            dtInt = "O";
+        String mnt = dt[1];
+        String yr = dt[0].substring(2);
+        String fChar = "";
+        if (name.length() == 1)
+            fChar += "X";
+        if (name.length() == 2)
+            fChar += name;
+        else if (name.equals(""))
+            fChar = "XX";
+        else
+            fChar = name.substring(0, 1);
+        
+        String lChar = "";
+        if (name.length() == 1)
+            lChar += "X";
+        else if (name.length() == 2) 
+            lChar += name;
+        else if (name.equals(""))
+            lChar = "XX";
+        else
+            lChar = name.substring(name.length()-1);
+        String id = yr+"-"+mnt+"-"+dtInt+"-";
+        if (name.equals(""))
+            id += "XX";
+        else if (name.length() == 2)
+            id += name;
+        else
+            id += fChar.toUpperCase() + lChar.toUpperCase();
+        
+        return id;
     }
 
     public Date getdateadd()
@@ -34,6 +79,6 @@ public class details
     }
     public void displays_details()
     {
-        System.out.println(" \n DateAdd : " + dateAdd);
+        System.out.println("Id : " + id + " DateAdd : " + dateAdd);
     }
 }
---
+++ test/Testcategory.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import team4.category;
+
+/**
+ *
+ * @author temp
+ */
+public class Testcategory {
+    
+    public Testcategory() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCraetedId()
+    {
+        System.out.println("Test 1 for Category method");
+        String name="Tourists";
+       // Date date = new Date();
+        String expectedResult = "19-Mar-O-TS";
+        String realResult = category.createID(name);
+         assertEquals(expectedResult ,realResult);
+        
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
